634b9db6ff067a523c8694778a6a8c0124cc4be8
alampranto17 <alampranto17@gmail.com>
2026-02-19 00:04:14 +0600

    alam

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'hlw world too';
+    return 'hlw world this is alam';
   }
 }
