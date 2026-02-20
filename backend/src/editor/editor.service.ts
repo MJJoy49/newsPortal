933413ede52a3468c28d08ccf3b487a1b6243d4b
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2026-02-20 17:33:15 +0600

    feat: refactor EditorService methods to return structured objects and enhance clarity

--- backend/src/editor/editor.service.ts
+++ backend/src/editor/editor.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNewsDto } from './dto/create-news.dto';
+import { ApproveNewsDto } from './dto/approve-news.dto';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateNewsDto } from './dto/update-news.dto';
 
 @Injectable()
 export class EditorService {
-  createNewsAndPublish(createNewsDto: CreateNewsDto) {
-    return 'This action create and publish a news';
+  createNewsAndPublish(createNewsDto: CreateNewsDto): object {
+    return { message: "This action will create a news", title: createNewsDto.title, breaking: createNewsDto.isBreaking };
+  }
+  showNewsList(status?: string, categoryId?: number, page?: number, limit?: number): object {
+    return { message: 'This action will show news list', status: status, categoryId: categoryId, page: page, limit: limit }
+  }
+  showNewsById(id: string): object {
+    return { message: `This will show news ${id}` }
+  }
+  updateNews(id: string, updateNewsDto: UpdateNewsDto): object {
+    return {message: `this action will update news ${id}, ${updateNewsDto.title}`}
+  }
+  deleteNewsById(id: string): object {
+    return { message: `This action will delete news ${id}`}
+  }
+  approveOrReject(id: string, approveNewsDto: ApproveNewsDto): object {
+    return { message: `This action will ${approveNewsDto.status} news ${id} ` }
+  }
+  createCategory(createCategoryDto: CreateCategoryDto): object {
+    return { message: `This will create a category ${createCategoryDto.name}` }
+  }
+  deleteCategoryById(id: string): object {
+    return { message: `This will delete category ${id}` }
   }
 }
