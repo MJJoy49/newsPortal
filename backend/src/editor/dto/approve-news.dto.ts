5f09b0348f4777b04194f1e33c1955035fa83db8
MJJoy49 <mdjoy1477@gmail.com>
2026-02-22 22:49:40 +0600

    Remove editor folder from reporter branch.

--- backend/src/editor/dto/approve-news.dto.ts
+++
@@ -1,4 +0,0 @@
-export class ApproveNewsDto {
-  status: string;
-  rejectionReason?: string;
-}
--- backend/src/editor/dto/create-news.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateNewsDto {
-  title: string;
-  subtitle?: string;
-  content: string;
-  categoryId: number;
-  tagsId?: number[];
-  thumbnailUrl?: string;
-  isBreaking?: boolean;
-}
