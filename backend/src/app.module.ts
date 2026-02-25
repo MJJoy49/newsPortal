587ba5708829bd9a38abb98f25ea993a56c47e5f
alampranto17 <alampranto17@gmail.com>
2026-02-25 16:38:28 +0600

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReporterModule } from './reporter/reporter.module';
-import { EditorModule } from './editor/editor.module';
-
 
 @Module({
-  imports: [EditorModule, ReporterModule],
+  imports: [ReporterModule],
   controllers: [AppController],
   providers: [AppService],
 })
